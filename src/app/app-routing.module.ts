f79714582a3ec8b39a8eccf0ab7ae4414d8a7a12
Raviteja Gandam <gandamraviteja@gmail.com>
2024-05-17 11:02:20 +0530

    intitial setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './fruit/home/home.component';
+import { EditComponent } from './fruit/edit/edit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'fruit/index',component:HomeComponent},
+  {path:'fruit',redirectTo:'fruit',pathMatch:'full'},
+  {path:'',redirectTo:'fruit',pathMatch:'full'},  
+  {path:'edit',component:EditComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateComponent } from './fruits/create/create.component';
+import { EditComponent } from './fruits/edit/edit.component';
+import { HomeComponent } from './fruits/home/home.component';
+import {HttpClientModule} from '@angular/common/http'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateComponent,
+    EditComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
